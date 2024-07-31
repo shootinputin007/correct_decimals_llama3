bfdd9d8420ec28b0640a630b98886c1a293dc91c
Pavle Kerkez <100360636+shootinputin007@users.noreply.github.com>
2024-07-31 15:30:02 +0200

    Update correct_decimals_llama3.py

--- correct_decimals_llama3.py
+++ correct_decimals_llama3.py
@@ -1,5 +1,6 @@
 import json
 import os 
+import time
 from groq import Groq
 from dotenv import load_dotenv
 from google.cloud import storage
@@ -12,6 +13,7 @@ bucket_name='tx_explain'
 storage_client = storage.Client()
 bucket = storage_client.bucket(bucket_name)
 
+# Get sim_data from buckets
 def get_simData (network, tx_hash):
     sim_blob = bucket.blob(f'{network}/transactions/simulations/trimmed/{tx_hash}.json')
     if sim_blob.exists():
@@ -21,6 +23,7 @@ def get_simData (network, tx_hash):
         print('Simulation not in buckets.')
         return None
 
+# Get explanation from buckets
 def get_explanationData (tx_hash):
     expl_blob = bucket.blob(f'ethereum/transactions/explanations/{tx_hash}.json')
     if expl_blob.exists():
@@ -29,12 +32,12 @@ def get_explanationData (tx_hash):
     else:
         print('Explanation not in buckets.')
         return None
-    
+
+# Extract correct amounts from sim_data
 def assets_data (cached_sim_data):
     asset_amounts = [x['amount'] for x in cached_sim_data['asset_changes']]
     asset_names = [x['token_info']['name'] for x in cached_sim_data['asset_changes']]
     asset_symbols = [x['token_info']['symbol'] for x in cached_sim_data['asset_changes']]
-    #asset_decimals = [x['token_info']['decimals']  for x in cached_sim_data['asset_changes']]
 
     assets = [
         {
@@ -49,7 +52,7 @@ def assets_data (cached_sim_data):
 
     return  assets_json
 
-
+# Run groq model
 def run_model (prompt, model = 'llama3-70b-8192'):
 
     GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
@@ -64,6 +67,7 @@ def run_model (prompt, model = 'llama3-70b-8192'):
     except Exception as e:
         print("Error at run_model: ", e)
 
+# Correct decimals in transaction summary
 def correct_summary (cached_sim_data, cached_expl_data):
     assets_json = assets_data (cached_sim_data)
 
@@ -90,19 +94,15 @@ def correct_summary (cached_sim_data, cached_expl_data):
     return modified_expl_data
 
 
-
-
+# Testing
 tx_hash = '0x090e79fd0c0c2cbd1d5573310a58f0cff93a14c9a1d035e31ab1be8a7eeae58c'
 network = 'arbitrum'
 
 cached_sim_data = get_simData (network, tx_hash)
 cached_expl_data = get_explanationData (tx_hash)
 
-import time
 start_time = time.time()
-
 modified_expl_data = correct_summary(cached_sim_data, cached_expl_data)
-
 end_time = time.time()
 elapsed_time = end_time - start_time
 print(f"Time taken to run the function: {elapsed_time:.2f} seconds")
